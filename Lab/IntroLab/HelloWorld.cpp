998e3146a03c11a0ce7a9a783e71fa04b5788d97
Miguel Belarmino <miguelbelarmino@Mac-7391.lan>
2025-08-28 01:24:39 -0700

    Added IntroLab

---
+++ Lab/IntroLab/HelloWorld.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: J Miguel Belarmino
+ * Created on August 28th, 2025, 12:07 AM
+ * Purpose:  Hello World
+ */
+
+//System Libraries
+#include <iostream>  //Input Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants not Variables
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+
+//Execution begins here at main
+int main(int argc, char** argv) {
+    //Set random number seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map/Process the Inputs -> Outputs
+    
+    //Display Inputs/Outputs
+    cout << "Hello World" << endl;
+    //Clean up memory and files
+    
+    //Exit the Program
+    return 0;
+}
