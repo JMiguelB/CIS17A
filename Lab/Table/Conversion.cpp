71b07b3a5b3018670e9896cd66eeafdcfa449a14
Miguel Belarmino <miguelbelarmino@Mac-270.lan>
2025-09-04 22:48:20 -0700

    Added Flowchart folder

---
+++ Lab/Table/Conversion.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: J Miguel Belarmino
+ * Created on Aug 29, 2025, 12:40 AM
+ * Purpose:  Lab Conversion table
+ *           base 2,8,10,16
+ */
+
+//System Libraries
+#include <iostream>  //Input Output Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants not Variables
+//Science, Math, Conversions, Dimensions
+
+//Function Prototypes
+n begins here at main
+int main(
+//Executioint argc, char** argv) {
+    //Set random number seed
+    
+    //Declare Variables
+    int n,rsult;
+    //Initialize Variables
+    cin >> n;
+    //Map/Process the Inputs -> Outputs
+    rsult=(n/16)%16;
+    //Display Inputs/Outputs
+    cout<<rsult<<endl;
+    //Clean up memory and files
+    
+    //Exit the Program
+    return 0;
+}
